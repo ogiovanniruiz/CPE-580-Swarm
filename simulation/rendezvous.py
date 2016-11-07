eda88ed468f1b19707e48f007a2081cfaee005bb
ogiovanniruiz <ogiovanniruiz@gmail.com>
2016-11-06 22:25:48 -0800

    Leader sim with class architecture

--- simulation/rendezvous.py
+++ simulation/rendezvous.py
@@ -2,8 +2,6 @@ import pygame
 from pygame.locals import *
 
 import numpy as np
-import scipy.spatial.distance
-
 
 red = [255, 0, 0]
 green = [0, 255, 0]
@@ -11,64 +9,77 @@ blue = [0, 0, 255]
 white = [255, 255, 255]
 black = [0, 0, 0]
 
-if __name__ == '__main__':
-	N = 20# Number of Robots
-	D = 2  # Dimension of search space
-	c = 0.0005 # iteration rate
-	P = 800 * np.random.rand(D, N) # Initial positions of robots in a 10x10 unit space
-	Pn = np.zeros((D,N), dtype=np.int)# Position bucket I THINK...
+SCREENSIZE = [800, 800]  # Size of our output display
+screenBGColor = black  # White background
 
-	P0 = 800 * np.random.rand(D,1)
-	P0n = np.zeros((D,1), dtype=np.int)
-	Y = [500,500]
+N = 20  # Number of Robots
+D = 2  # Dimension of search space
+c = 0.0005  # iteration rate
+c0 = 0.005 # leader iteration rate
+Y = [500, 500] # final position of leader
+goal = 1  # The final error should be really small
+running = True
 
-	error = float("inf") # Initial error is HUGE. We want to minimize this.
-	goal = 0.001# The final error should be really small
-	steps = 0 # TIME STEPS cuz computer reasons.
+class Swarm_Simulation:
+	def __init__(self):
 
-	SCREENSIZE = [800, 800] # Size of our output display
-	screenBGColor = black # White background
-	running = True
+		pygame.init()  # Shall we begin?
+		self.screen = pygame.display.set_mode(SCREENSIZE)
+		self.screen.fill(screenBGColor)
+		pygame.display.set_caption("Rendezvous Simulation")
 
-	pygame.init() # Shall we begin?
-	screen = pygame.display.set_mode(SCREENSIZE)
-	screen.fill(screenBGColor)
-	pygame.display.set_caption("Rendezvous Simulation")
+		self.error = float("inf")  # Initial error is HUGE. We want to minimize this.
+		self.steps = 0
 
-	try:
-		while(running):
+		self.P = 800 * np.random.rand(D, N)  # Initial positions of robots in a 10x10 unit space
+		self.Pn = np.zeros((D, N), dtype=np.int)  # Position bucket I THINK...
 
-			while error > goal:
-				Pn = P
-				P0n = P0
-				steps += 1
 
-				P0n = P0n - c* (P0 -Y)
-				pygame.draw.circle(screen, black, [int(P0[0, 0]), int(P0[1, 0])], 7, 1)
+		self.P0 = 800 * np.random.rand(D, 1)
+		self.P0n = np.zeros((D, 1), dtype=np.int)
+
+
+	def Run(self):
+
+
+
+		while (running):
+
+			while self.error > goal:
+				self.Pn = self.P
+				self.P0n = self.P0
+				self.steps += 1
+
+				self.P0n = self.P0n - c0 * (self.P0 - Y)
+				pygame.draw.circle(self.screen, black, [int(self.P0[0, 0]), int(self.P0[1, 0])], 7, 1)
 
 				for i in range(N):
 					for j in range(N):
-						pygame.draw.circle(screen, black, [int(P[0, i]), int(P[1, i])], 7, 1)
-						#pygame.draw.circle(screen, black, [int(P0[0, i]), int(P0[1, i])], 7, 1)
+						pygame.draw.circle(self.screen, black, [int(self.P[0, i]), int(self.P[1, i])], 7, 1)
 
-						Pn[:, i] = Pn[:, i] + c * (P[:, j]-P[:, i])  - c * (P[:,j] - P0[:,0])
+						self.Pn[:, i] = self.Pn[:, i] + c * 0.1 * (self.P[:, j] - self.P[:, i]) - c * (self.P[:, j] - self.P0[:, 0])
 
-						pygame.draw.circle(screen, green, [int(P[0,i]),int(P[1,i])] ,7, 1)
-						#pygame.draw.circle(screen, red, [int(P0[0, 0]), int(P0[1, 0])], 7, 1)
+						pygame.draw.circle(self.screen, green, [int(self.P[0, i]), int(self.P[1, i])], 7, 1)
 
+				self.P = self.Pn
+				self.P0 = self.P0n
+				pygame.draw.circle(self.screen, red, [int(self.P0[0, 0]), int(self.P0[1, 0])], 7, 1)
 
-				P = Pn
-				P0 = P0n
-				pygame.draw.circle(screen, red, [int(P0[0, 0]), int(P0[1, 0])], 7, 1)
-				n_error = np.linalg.norm(P0n -Y)
+				n_error = np.linalg.norm(self.P0n - Y)
 
-				error = n_error
-				print ('ERROR: ', error)
+				self.error = n_error
+				print('ERROR: ', self.error)
 				pygame.display.update()
 
-
-			print ("Consensus Reached in ", steps, "steps")
+			print("Consensus Reached in ", self.steps, "steps")
 			input("Ctrl+C to exit")
+
+
+if __name__ == '__main__':
+
+	try:
+		sim	= Swarm_Simulation()
+		sim.Run()
 
 	except KeyboardInterrupt:
 		print (" Shutting down simulation...")
