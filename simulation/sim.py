166603d690f200ca99f4ade45d1773f2573a8a1e
aaron <aarontpze@yahoo.com>
2016-11-16 10:43:25 -0800

    Added inheritance so bots are cleaner, re-added MultiNEAT (it can easily be commented out), updated rangefinder so bots can additionally detect color of what they're looking at.

--- simulation/sim.py
+++ simulation/sim.py
@@ -11,6 +11,16 @@ from abc import abstractmethod
 #from keras.layers import Dense, Activation
 
 
+red = [255,0,0]
+green = [0,255,0]
+blue = [0,0,255]
+white = [255,255,255]
+black = [0,0,0]
+UP = [0,-1]
+DOWN = [0,1]
+LEFT = [-1,0]
+RIGHT = [1,0]
+NOTMOVING = [0,0]
 
 
 class Controller:
@@ -29,9 +39,99 @@ class DumbController:
         return [0, 0]
 
 
-class KerasWrapper:
-    pass
 
+## IF YOU DON'T HAVE MULTINEAT JUST COMMENT THIS OUT, DON'T REMOVE IT
+import MultiNEAT as NEAT
+class MultiNEATWrapper:
+    """The NEATWrapper contains the means of initializing a NEAT
+    network with a set of specific parameters, and the means of getting
+    a Network at every timestep which represents the most fit (and
+    currently tested) NEAT genome, and the means of updating the fitness
+    of the NEAT genomes as they go."""
+    def __init__(self, params, genome, population = None, 
+                    *args, **kwargs):
+        self.params = params
+        if population is None:
+            population = Population(genome, params, True, 2.0, 1)    
+        self.population = population
+        self.current = 0
+        self.genomes = None
+
+
+    def get_best_genome(self):
+        return self.population.GetBestGenome()
+
+    def get_current_genome(self, progress = True):
+        if self.genomes is None:
+            self.genomes = NEAT.GetGenomeList(self.population)
+        if self.current >= len(self.genomes):
+            self.update()
+        genome = self.genomes[self.current]
+        if progress: self.current += 1
+        return genome
+            
+    def set_current_fitness(self, fit):
+        self.get_current_genome().SetFitness(fit)
+
+    def update(self):
+        print " ------------------------------ CURRENT BEST FITNESS: ", self.population.GetBestGenome().GetFitness()
+        print " ------------------------------ BEST FITNESS EVER: ", self.population.GetBestFitnessEver()
+        self.population.Epoch()
+        self.genomes = NEAT.GetGenomeList(self.population)
+        self.current = 0
+
+    def copy(self): #returns a NEW OBJECT identical to current one
+        Ellipsis
+
+class MultiNEATController:
+    def __init__(self, wrapper, num_bots, steps = 100):
+        self.wrapper = wrapper
+        self.genome = None
+        self.networks = {} 
+        for i in range(num_bots):
+            self.networks[i] = NEAT.NeuralNetwork()
+        self.max_steps = steps
+        self.fitness = 0
+        self.step = 0
+
+    def __call__(self, senses, bot_id):
+        if self.step == 0:
+            self.genome = self.wrapper.get_current_genome()
+            for net in self.networks.values():
+                self.genome.BuildPhenotype(net) #theoretically build network 
+        net = self.networks[bot_id]
+        self.step += 1
+        self.step %= self.max_steps 
+        net.Input(senses)
+        net.Activate()
+        output = net.Output()
+        o = []
+        for i in range(len(output)):
+            if output[i] > 0.7:
+                o.append(1)
+            elif output[i] < 0.3:
+                o.append(-1)
+            else:
+                o.append(0)
+        self.tick()
+        return o 
+
+    def tick(self):
+        self.step += 1
+        self.step %= self.max_steps
+
+    def _feedback(self, *args): #TODO: Currently only takes a single bot's fitness
+        if self.step == 0:
+            self.fitness = args[0] 
+            self.wrapper.set_current_fitness(self.fitness) #assumes 1-element feedback
+            self.fitness = 0
+##
+
+
+
+class KerasWrapper:
+    def __init__():
+        pass
 
 
 class KerasController: 
@@ -39,41 +139,54 @@ class KerasController:
 
 
 
-
-
-
-
-class TankDriveBot:
-    def __init__(self, x, y, speed, rotate_rate, orientation, controller, los_range = 130):
+class RangefinderBot:
+    def __init__(self, x, y, speed, rotate_rate, orientation, controller, los_range = 130, rect_h = 16, rect_w = 16, color = blue):
         self.x = x
         self.y = y
+        self.rect_h = rect_h
+        self.rect_w = rect_w
+        self.color = color
         self.orientation = orientation
         self.speed = speed
         self.rotate_rate = rotate_rate
         self.controller = controller
-        self.rect = pygame.Rect(self.x,self.y,16,16)
+        self.rect = pygame.Rect(self.x,self.y,rect_h,rect_w)
         self.los_range = los_range 
         self.los = None
         
-        self.stationary_time = 0
-        self.rotate_time = 0
-        self.rotate_direction = 0
-
+    def setpos(self,x,y):
+        self.x = x
+        self.y = y
+    
     def draw(self, screen):
         #pygame.draw.ellipse(self.surface, black, self.surface.get_rect(), 2)
         if True: #to render rangefinder distance
             font = pygame.font.Font(None, 16)
             surface = font.render(str(self.rangefinder()), 0, blue)
             screen.blit(surface, (self.x, self.y))
-        pygame.draw.circle(screen, blue, (int(floor(self.x)), int(floor(self.y))), 7, 1)
-        self.los = pygame.draw.line(screen, black, (self.x, self.y), (self.x + sin(self.orientation) * self.los_range, 
+        pygame.draw.circle(screen, self.color, (int(floor(self.x)), int(floor(self.y))), 7, 1)
+        self.los = pygame.draw.line(screen, white, (self.x, self.y), (self.x + sin(self.orientation) * self.los_range, 
                 self.y + cos(self.orientation) * self.los_range))
         #screen.blit(self.surface, (self.x, self.y))
+    def rangefinder(self): 
+        """Raycasts based on orientation / position of the bot, returns the distance from the closest object.
+        
+        For the moment performs a VERY costly computation of iterating over a distance."""
+        color = [255, 255, 255]
+        distance = float('inf')
+        if self.los is not None:
+            for c in collidables:
+                if c != self and self.los.colliderect(c.rect): #Collision detected
+                    color = c.color 
+                    DIST_STEP = 1 
+                    #Now we test a discrete set of distances from the origin (bot) to the end of the los
+                    for i in np.arange(0, self.los_range, DIST_STEP): #iterate from 0 to range in DIST_STEP steps
+                        point = (self.x + sin(self.orientation) * i, self.y + cos(self.orientation) * i)
+                        if c.rect.collidepoint(point[0], point[1]):    
+                            return float(i), color
+        return distance, color 
 
-    def setpos(self,x,y):
-        self.x = x
-        self.y = y
-
+class TankDriveBot(RangefinderBot):
     def move(self, l_wheel, r_wheel):
         stationary_flag = False
         collision_flag = False
@@ -118,105 +231,15 @@ class TankDriveBot:
         if self.rotate_time * self.rotate_rate >= ROTATE_THRESHOLD:
             rotate_flag = True
         return (collision_flag, stationary_flag, rotate_flag, invalid_reverse_flag)
-            
 
-
-    def rangefinder(self): 
-        """Raycasts based on orientation / position of the bot, returns the distance from the closest object.
-        
-        For the moment performs a VERY costly computation of iterating over a distance."""
-        distance = float('inf')
-        if self.los is not None:
-            for c in collidables:
-                if c != self and self.los.colliderect(c.rect): #Collision detected
-                    DIST_STEP = 1 
-                    #Now we test a discrete set of distances from the origin (bot) to the end of the los
-                    for i in np.arange(0, self.los_range, DIST_STEP): #iterate from 0 to range in DIST_STEP steps
-                        point = (self.x + sin(self.orientation) * i, self.y + cos(self.orientation) * i)
-                        if c.rect.collidepoint(point[0], point[1]):    
-                            return float(i)
-        return distance
-
-class LinearBot:
-    def __init__(self, x, y, speed, rotate_rate, orientation, controller, los_range = 130, rect_h = 16, rect_w = 16):
-        self.x = x
-        self.y = y
-        self.rect_h = rect_h
-        self.rect_w = rect_w
-        self.orientation = orientation
-        self.speed = speed
-        self.rotate_rate = rotate_rate
-        self.controller = controller
-        self.rect = pygame.Rect(self.x,self.y,rect_h,rect_w)
-        self.los_range = los_range 
-        self.los = None
-        
-    def setpos(self,x,y):
-        self.x = x
-        self.y = y
-    
-    def draw(self, screen):
-        #pygame.draw.ellipse(self.surface, black, self.surface.get_rect(), 2)
-        if True: #to render rangefinder distance
-            font = pygame.font.Font(None, 16)
-            surface = font.render(str(self.rangefinder()), 0, blue)
-            screen.blit(surface, (self.x, self.y))
-        pygame.draw.circle(screen, blue, (int(floor(self.x)), int(floor(self.y))), 7, 1)
-        self.los = pygame.draw.line(screen, black, (self.x, self.y), (self.x + sin(self.orientation) * self.los_range, 
-                self.y + cos(self.orientation) * self.los_range))
-        #screen.blit(self.surface, (self.x, self.y))
-    
+class LinearBot(RangefinderBot):
     def move(self, v, h, delta_orientation):
-        #collision = {}
-        #hit_wall = False
-        #self.orientation += delta_orientation * self.rotate_rate
-        #self.orientation %= (2 * pi) 
-        #tmp_x = self.x + h * self.speed
-        #tmp_y = self.y + v * self.speed
-        #tmp_rect = pygame.Rect(tmp_x, tmp_y, 16, 16)
-        #for c in collidables:
-        #    if tmp_rect.colliderect(c.rect) and c != self:
-        #        if hasattr(c, 'moveable') and c.moveable == True: #collided with moveable object
-        #            collision[c] = (h * sin(self.orientation) * self.speed, v * cos(self.orientation) * self.speed)
-        #        else: hit_wall = True
-
-        #if not len(collision) and not hit_wall:
-        #    self.x = tmp_x
-        #    self.y = tmp_y
-        #self.rect = pygame.Rect(self.x, self.y, self.rect_h, self.rect_w)
-        #return collision    
         self.orientation += delta_orientation * self.rotate_rate
         return (h * self.speed, v * self.speed)
 
-    def rangefinder(self): 
-        """Raycasts based on orientation / position of the bot, returns the distance from the closest object.
-        
-        For the moment performs a VERY costly computation of iterating over a distance."""
-        distance = float('inf')
-        if self.los is not None:
-            for c in collidables:
-                if c != self and self.los.colliderect(c.rect): #Collision detected
-                    DIST_STEP = 1 
-                    #Now we test a discrete set of distances from the origin (bot) to the end of the los
-                    for i in np.arange(0, self.los_range, DIST_STEP): #iterate from 0 to range in DIST_STEP steps
-                        point = (self.x + sin(self.orientation) * i, self.y + cos(self.orientation) * i)
-                        if c.rect.collidepoint(point[0], point[1]):    
-                            return float(i)
-        return distance
-
-red = [255,0,0]
-green = [0,255,0]
-blue = [0,0,255]
-white = [255,255,255]
-black = [0,0,0]
-UP = [0,-1]
-DOWN = [0,1]
-LEFT = [-1,0]
-RIGHT = [1,0]
-NOTMOVING = [0,0]
 
 class Environment:
-    def __init__(self, shape, speed, controllers, bots, collidable, feedback_func = None, player = []):
+    def __init__(self, shape, speed, controllers, bots, collidable, movement_func = None, feedback_func = None, penalty_func = None, player = []):
         self.speed = speed 
         #self.single_controller = (len(controllers) == 1) #should all bots operate on the same controller / GA?
         self.controllers = controllers
@@ -226,7 +249,7 @@ class Environment:
         self.shape = shape
 
         pygame.init()
-        self.screenBGColor = white
+        self.screenBGColor = black
         self.screen=pygame.display.set_mode(self.shape)
         pygame.display.set_caption("CRAP")
         self.clock=pygame.time.Clock()
@@ -237,7 +260,9 @@ class Environment:
 
         self.feedback = 0 #the feedback whatever controller in the simulation will be operating on
 
-
+        self.feedback_func = feedback_func
+        self.movement_func = movement_func
+        self.penalty_func = penalty_func
         ##Extraneous functionality
         pygame.font.init()
 
@@ -296,10 +321,10 @@ class Environment:
             bot.controller._feedback(self.feedback)
 
         collidables = []
-        collidables.append(collidable(0, 0, SCREENSIZE[0], 3, blue))
-        collidables.append(collidable(0, 0, 3, SCREENSIZE[1], blue))
-        collidables.append(collidable(SCREENSIZE[0] - 1, 0, 3, SCREENSIZE[1], blue))
-        collidables.append(collidable(0, SCREENSIZE[1] - 1, SCREENSIZE[0], 3, blue))
+        collidables.append(collidable(0, 0, SCREENSIZE[0], 3, white))
+        collidables.append(collidable(0, 0, 3, SCREENSIZE[1], white))
+        collidables.append(collidable(SCREENSIZE[0] - 1, 0, 3, SCREENSIZE[1], white))
+        collidables.append(collidable(0, SCREENSIZE[1] - 1, SCREENSIZE[0], 3, white))
         #initialize inner walls, if necessary
         #def initialize_collidable_obstacle(ENV_SHAPE, num, mean_width, w_var, mean_height, h_var, min_space = 100):
         collidable_initializer = initialize_collidable_obstacles(SCREENSIZE, 28, 100, 40, 3, 0, 70)
@@ -328,37 +353,12 @@ class Environment:
         collision = {}
         for bot in self.bots:
             if bot.controller:
-                hit_wall = False
-                instructions = bot.controller([bot.rangefinder(), 1.0]) #TODO: Add sensory inputs here
-                res = bot.move(instructions[0], instructions[1], instructions[2])
-                tmp_x = bot.x + res[0]
-                tmp_y = bot.y + res[1]
-                tmp_rect = pygame.Rect(tmp_x, tmp_y, bot.rect_h, bot.rect_w)
-                for c in collidables:
-                    if tmp_rect.colliderect(c.rect) and c != bot:
-                        if hasattr(c, 'moveable') and c.moveable == True: #collided with moveable object
-                            collision.setdefault(c, []).append((res[0], res[1]))
-                        else: hit_wall = True
-                if not len(collision) and not hit_wall:
-                    bot.x = tmp_x
-                    bot.y = tmp_y
-                bot.rect = pygame.Rect(bot.x, bot.y, bot.rect_h, bot.rect_w)
+                dist, color = bot.rangefinder() #gets distance and color from rangefinder, now
+                instructions = bot.controller([bot.rangefinder(), color[0], color[1], color[2], 1.0]) #TODO: Add sensory inputs here
+                collision = self.movement_func(self, bot, instructions, collision)
         if self.player:
             for player in self.player:
-                res = player.move(*self.player_instructions)
-                hit_wall = False
-                tmp_x = player.x + res[0]
-                tmp_y = player.y + res[1]
-                tmp_rect = pygame.Rect(tmp_x, tmp_y, player.rect_h, player.rect_w)
-                for c in collidables:
-                    if tmp_rect.colliderect(c.rect) and c != player:
-                        if hasattr(c, 'moveable') and c.moveable == True: #collided with moveable object
-                            collision.setdefault(c, []).append((res[0], res[1]))
-                        else: hit_wall = True
-                if not len(collision) and not hit_wall:
-                    player.x = tmp_x
-                    player.y = tmp_y
-                player.rect = pygame.Rect(player.x, player.y, player.rect_h, player.rect_w)
+                collision = self.movement_func(self, player, self.player_instructions, collision)
         
         for c in range(len(collision)):
             net_x = sum([i[0] for i in collision.values()[c]])
@@ -447,13 +447,27 @@ def initialize_collidable_obstacles(ENV_SHAPE, num, mean_width, w_var, mean_heig
     for x, y in pos:
         height = mean_height + h_var * random.random() * random.choice((1, -1))
         width = mean_width + w_var * random.random() * random.choice((1, -1))
-        collidables.append(collidable(x, y, height, width, blue))
+        collidables.append(collidable(x, y, height, width, white))
     return collidables
 
+def move_bots(env, bot, instructions = [], collision = []):
+    hit_wall = False
+    res = bot.move(instructions[0], instructions[1], instructions[2])
+    tmp_x = bot.x + res[0]
+    tmp_y = bot.y + res[1]
+    tmp_rect = pygame.Rect(tmp_x, tmp_y, bot.rect_h, bot.rect_w)
+    for c in collidables:
+        if tmp_rect.colliderect(c.rect) and c != bot:
+            if hasattr(c, 'moveable') and c.moveable == True: #collided with moveable object
+                collision.setdefault(c, []).append((res[0], res[1]))
+            else: hit_wall = True
+    if not len(collision) and not hit_wall:
+        bot.x = tmp_x
+        bot.y = tmp_y
+    bot.rect = pygame.Rect(bot.x, bot.y, bot.rect_h, bot.rect_w)
+    return collision
 
-
-
-def feedback_by_distances(environment):
+def feedback_by_relative_bot_distances(environment, *args):
         for i in range(len(self.bots)): #for now we just space them horizontally 
             closest = float('inf')
             for j in range(len(self.bots)): #TODO NOW WE MIMIZE DISTANCE TO CLOSEST BOT
@@ -462,6 +476,13 @@ def feedback_by_distances(environment):
                     if distance < closest:
                         closest = distance
             self.feedback += closest #should never be inf
+
+def feedback_by_moveable_distance_to_region(environment, region): #region is a Rect 
+    pass
+
+
+def feedback_by_total_bot_distance_from_region(environment, region):  #region is a Rect
+    pass
 
 
 if __name__ == '__main__':
@@ -517,8 +538,8 @@ if __name__ == '__main__':
             factor = sqrt(d_X ** 2 + d_Y **2) / float(self.inertia)
             if factor > 1: #force overcomes inertia, move moveable
                 collision_flag = False
-                tmp_x = self.x + d_X * factor
-                tmp_y = self.y + d_Y * factor
+                tmp_x = self.x + d_X * (1 / factor)
+                tmp_y = self.y + d_Y * (1 / factor)
                 tmp_rect = pygame.Rect(tmp_x, tmp_y, self.w, self.h)
                 for c in collidables:
                     if tmp_rect.colliderect(c.rect) and c != self:
@@ -543,9 +564,10 @@ if __name__ == '__main__':
     collidables.extend(collidable_initializer) 
     #
     collidables.extend(bots)
-    collidables.extend([moveable(4.5, random.random() * 700, random.random() * 700, 20, 20, green) for i in range(7)])
+    collidables.extend([moveable(4.5, random.random() * 700, random.random() * 700, 30, 30, green) for i in range(7)])
     if len(players): collidables.extend(players)
-    env = Environment(SCREENSIZE, SPEED, controller, bots, collidables, feedback_func = feedback_by_distances, player = players)
+    env = Environment(SCREENSIZE, SPEED, controller, bots, collidables, feedback_func = feedback_by_relative_bot_distances, 
+            movement_func = move_bots, player = players)
     env.play() 
 
 
