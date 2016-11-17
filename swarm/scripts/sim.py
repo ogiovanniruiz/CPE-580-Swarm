e73d2f96fad7516425262a8d8f735aecb90babe6
ogiovanniruiz <ogiovanniruiz@gmail.com>
2016-11-16 20:24:17 -0800

    SIM is read

--- swarm/scripts/sim.py
+++ swarm/scripts/sim.py
@@ -17,11 +17,11 @@ black = [0, 0, 0]
 SCREENSIZE = [1500, 1000]  # Size of our output display
 screenBGColor = black  # White background
 
-N = 20  # Number of Robots
+N = 50  # Number of Robots
 D = 2  # Dimension of search space
-M = 0# Number of Obstacles
-c = 0.0005  # iteration rate
-c0 = 0.005  # leader iteration rate
+M = 20# Number of Obstacles
+c = 0.005  # iteration rate
+dt = 5  # leader iteration rate
 
 A = np.ones((N,N), dtype=np.int)#np.random.rand(N,N) #Agacency Matrix is Randomized for now
 
@@ -49,60 +49,86 @@ class Swarm_Simulation:
         self.P0 = 1000 * np.random.rand(D, 1)  #Initial position of RED Leader
         self.P0n = np.zeros((D, 1), dtype=np.int) # Position bucket for RED Leader
 
-        self.vertical = 0 #Joy stick controls
-        self.horizontal = 0
+        self.vel_y = 0 #Joy stick controls
+        self.vel_x = 0
 
-        self.target = [0,0]
-
-        self.r = rospy.Rate(10)
+        self.rand_speeds = np.random.rand(D,N)
 
         for i in range(N):
 
-            bots.append(Circles(self.Pn[0,i], self.Pn[1,i], 3, 3, green))
+            bots.append(Circles(self.Pn[0,i], self.Pn[1,i], 30, 30, green))
 
-        #collidables.extend(bots)
-        Maze()
+        self.Maze()
+
         self.Run()
 
 
     def array_callback(self, twist_array):
 
-        self.vertical = -twist_array.linear.x
-        self.horizontal = twist_array.angular.z
+        self.vel_y = -twist_array.linear.x
+        self.vel_x = twist_array.angular.z
+
+        #print (self.vel_x)
 
     def Run(self):
         while not rospy.is_shutdown():
 
-            self.target = [[self.horizontal * 1000 + 750], [self.vertical * 1000 + 500]]  # final position of leader
-            self.Pn = self.P
             self.P0n = self.P0
-            self.red = pygame.Rect(self.P0[0,0],self.P0[1,0],2,2)
 
+            pygame.draw.circle(self.screen, black, [int(self.P0n[0, 0]), int(self.P0n[1, 0])], 7, 1)
 
-            self.P0n = self.P0n - c0 * (self.P0 - self.target)
+            self.P0n[:,0] = self.P0n[:,0] + [(self.vel_x *dt),(self.vel_y * dt)]
 
-            pygame.draw.circle(self.screen, black, [int(self.P0[0, 0]), int(self.P0[1, 0])], 7, 1)
+            self.red_leader = pygame.Rect(int(self.P0n[0, 0]), int(self.P0n[1, 0]), 7, 7)
+            self.los = pygame.Rect(int(self.P0n[0, 0]), int(self.P0n[1, 0]), 200, 200)
+
+            self.Pn = self.P
 
             for i in range(N):
-                for j in range(N):
-                    for k in range(len(collidables)):
+                for k in range(len(collidables)):
 
-                        pygame.draw.circle(self.screen, black, [int(self.Pn[0, i]), int(self.Pn[1, i])], 7, 1)
+                    if self.red_leader.colliderect(collidables[k].rect):
 
-                        if bots[i].rect.colliderect(collidables[k].rect):
-                            #print(bots[3].rect)
-                            print ("Collided")
-                            print(collidables[k].rect)
-                            self.Pn[:, i] = self.Pn[:, i] + c * (self.P[:, j] + self.P0[:, 0]) * A[i,j]
-                            #self.Pn[:, i] = self.Pn[:, i] - c * 0.01 * (self.P[:, j] + self.P[:, i]) * A[i,j] + c * (self.P[:, j] + self.P0[:, 0]) * A[i,j]
-                        else:
-                            self.Pn[:, i] = self.Pn[:, i] + c * (self.P[:, j] + self.P0[:, 0]) * A[i, j]
-                            #self.Pn[:, i] = self.Pn[:, i] + c * 0.01 * (self.P[:, j] - self.P[:, i]) * A[i, j] - c * (self.P[:, j] - self.P0[:, 0]) * A[i, j]
+                        self.P0n[:, 0] = self.P0n[:, 0] + [(-self.vel_x * dt), (-self.vel_y * dt)]
+                        self.red_leader = pygame.Rect(int(self.P0[0, 0]), int(self.P0[1, 0]), 10, 10)
+                        self.los = pygame.Rect(int(self.P0n[0, 0]), int(self.P0n[1, 0]), 35, 35)
 
-                        bots[i].rect.x = self.Pn[0,i]
-                        bots[i].rect.y = self.Pn[1,i]
-                        bots[i].x = self.Pn[0,i]
-                        bots[i].y = self.Pn[1,i]
+                    else:
+
+                        self.P0n = self.P0n
+                        self.red_leader = pygame.Rect(int(self.P0n[0, 0]), int(self.P0n[1, 0]), 10, 10)
+                        self.los = pygame.Rect(int(self.P0n[0, 0]), int(self.P0n[1, 0]), 35, 35)
+
+
+                    pygame.draw.circle(self.screen, black, [int(self.Pn[0, i]), int(self.Pn[1, i])], 7, 1)
+
+                    self.Pn[:, i] = self.Pn[:, i] + [self.rand_speeds[:, i] *0.1]
+
+                    bots[i].rect.x = self.Pn[0, i]
+                    bots[i].rect.y = self.Pn[1, i]
+
+                    if bots[i].rect.colliderect(self.los):
+                        self.Pn[:, i] = self.Pn[:, i] - c * (self.P[:, i] - self.P0[:, 0])
+                        self.rand_speeds[:,i] = -0.1 * self.rand_speeds[:,i]
+                        num_cap = len(self.los.collidelistall(bots))
+                        print (num_cap)
+                    else:
+                        self.Pn[:, i] = self.Pn[:, i] + [self.rand_speeds[:, i] * 0.1]
+
+
+                    if bots[i].rect.colliderect(collidables[k].rect):
+                        self.rand_speeds[:, i] = - self.rand_speeds[:,i]
+                        bots[i].rect.x = self.P[0, i]
+                        bots[i].rect.y = self.P[1, i]
+
+                    else:
+                        self.Pn[:, i] = self.Pn[:, i]
+                        bots[i].rect.x = self.P[0, i]
+                        bots[i].rect.y = self.P[1, i]
+
+
+                    bots[i].x = self.P[0,i]
+                    bots[i].y = self.P[1,i]
 
             for k in collidables:
                 k.draw(self.screen)  # Draw all obstacles
@@ -115,6 +141,32 @@ class Swarm_Simulation:
 
             pygame.draw.circle(self.screen, red, [int(self.P0[0, 0]), int(self.P0[1, 0])], 7, 1)
             pygame.display.update()
+
+    def Maze(self):
+
+        # Four Walls
+        collidables.append(Collidable(0, 0, SCREENSIZE[0], 10, white))
+        collidables.append(Collidable(0, 0, 10, SCREENSIZE[1], white))
+        collidables.append(Collidable(SCREENSIZE[0] - 4, 0, 10, SCREENSIZE[1], white))
+        collidables.append(Collidable(0, SCREENSIZE[1] - 4, SCREENSIZE[0], 10, white))
+
+        for x in range(M):
+            x = random.randint(0, SCREENSIZE[0])
+            y = random.randint(0, SCREENSIZE[1])
+
+            height = random.randint(9, 10)
+            width = random.randint(1, 500)
+            collidables.append(Collidable(x, y, height, width, white))
+
+        for x in range(M):
+            x = random.randint(0, SCREENSIZE[0])
+            y = random.randint(0, SCREENSIZE[1])
+
+            height = random.randint(1, 500)
+            width = random.randint(9, 10)
+            collidables.append(Collidable(x, y, height, width, white))
+
+
 
 
 class Collidable:
@@ -130,29 +182,7 @@ class Collidable:
         pygame.draw.rect(screen, self.color, [self.x, self.y, self.w, self.h], 6)
 
 
-def Maze():
 
-        #Four Walls
-        collidables.append(Collidable(0, 0, SCREENSIZE[0], 6, white))
-        collidables.append(Collidable(0, 0, 6, SCREENSIZE[1], white))
-        collidables.append(Collidable(SCREENSIZE[0] - 4, 0, 6, SCREENSIZE[1], white))
-        collidables.append(Collidable(0, SCREENSIZE[1] - 4, SCREENSIZE[0], 6, white))
-
-        for x in range(M):
-                x = random.randint(0,SCREENSIZE[0])
-                y = random.randint(0,SCREENSIZE[1])
-
-                height = random.randint(1,3)
-                width = random.randint(1,300)
-                collidables.append(Collidable(x, y, height, width, white))
-
-        for x in range(M):
-                x = random.randint(0,SCREENSIZE[0])
-                y = random.randint(0,SCREENSIZE[1])
-
-                height = random.randint(1,300)
-                width = random.randint(1,3)
-                collidables.append(Collidable(x, y, height, width, white))
 
 class Circles():
     def __init__(self, x, y, w, h, color):
@@ -164,8 +194,6 @@ class Circles():
         self.rect = pygame.Rect(x, y, w, h)
     def draw(self,screen):
         pygame.draw.circle(screen, self.color, [int(self.x), int(self.y)], 7, 1)
-
-
 
 def main(args):
     try:
